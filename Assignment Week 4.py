ae894f29e41a8a3e1231b2e1b158902a484501f7
Mayankbajeta1804 <127091702+Mayankbajeta1804@users.noreply.github.com>
2023-03-22 12:25:24 +0530

    Add files via upload

---
+++ Assignment Week 4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Mayank Singh Bajeta__
+# Data Architect
+
+# _I am intrested in Data Science because it is an emerging profession. And since the future will be of the IT industry I want to be up to date._
+
+# ### The code below will take 2 inputs from the user and multiply them.
+
+# In[3]:
+
+
+print("Enter a, and b and we will give you result 'a*b'.")
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+print("a*b = ", a*b)
+
+
+# ### Bullet List:
+# -  Bullet 1
+#     - Bullet 2
+#        - Bullet 3
+# ### Table:
+# | S.No. | Name | last Name |
+# | --- | --- | --- |
+# | 1 | Mayank | Bajeta |
+# | 2 | Rishabh | Panth |
+# ### Horizontal Rule:
+# ---
+# <hr>
+#        
